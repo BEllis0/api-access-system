6003f59158ee13b5bb7831d95b97370b98a2c008
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-04 10:28:59 -0600

    working on db connection

--- database/config.js
+++ database/config.js
@@ -1,29 +1,38 @@
 const { Pool, Client } = require('pg');
 
-const pool = new Pool({
-    user: 'localhost',
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.DB_PASS,
-    port: process.env.PORT,
-});
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-});
-
+var connectionString = "postgres://postgres:Skiclub0@localhost:5432/api_limiter";
 const client = new Client({
-  user: 'localhost',
-  host: process.env.HOST,
-  database: process.env.DB,
-  password: process.env.DB_PASS,
-  port: process.env.PORT,
+    connectionString: connectionString
 });
 
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
+client.connect((err) => {
+    err ? console.log(`Error connecting to db: ${err}`) : console.log('Successfully connected to database')
 });
+
+// const pool = new Pool({
+//     user: 'localhost',
+//     host: process.env.HOST,
+//     database: process.env.DB,
+//     password: process.env.DB_PASS,
+//     port: process.env.PORT,
+// });
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// });
+
+// const client = new Client({
+//   user: 'localhost',
+//   host: process.env.HOST,
+//   database: process.env.DB,
+//   password: process.env.DB_PASS,
+//   port: process.env.PORT,
+// });
+
+// client.connect();
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// });
