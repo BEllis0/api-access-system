2379e9740a66c4fd3f4e2711a1495d53559e885d
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-05 19:59:50 -0600

    updating schema

--- schema.sql
+++ schema.sql
@@ -1,27 +1,28 @@
--- Drop if exists
-DROP DATABASE IF EXISTS api_limiter;
+dropdb   api_limiter;
 
 -- Create db
 CREATE DATABASE api_limiter;
 
 USE api_limiter;
 
+CREATE SCHEMA api_limiter;
+
 -- users
 
-CREATE TABLE users (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+CREATE TABLE api_limiter.users (
+    id SERIAL PRIMARY KEY,
     date_added TIMESTAMP,
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     email_address VARCHAR(200) NOT NULL,
     membership_tier VARCHAR(50) default 'free',
     total_calls INT,
-
+    alotted_calls INT
 );
 
 -- table for each API call
 
-CREATE TABLE api_calls (
+CREATE TABLE api_limiter.api_calls (
     id SERIAL PRIMARY KEY,
     _date TIMESTAMP,
     _endpoint VARCHAR(500),
