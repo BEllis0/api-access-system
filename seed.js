0227300499f0a89a7befee2040dde7bb8f7082ae
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-06 15:16:14 -0600

    product seed func added

--- seed.js
+++ seed.js
@@ -1,12 +1,12 @@
 const faker = require('faker');
-const { insertCompanies } = require('./server/models/seedFunctions.js');
+const { insertCompanies, insertProducts } = require('./server/models/seedFunctions.js');
 const { truncateTables } = require('./server/models/truncateTables.js');
 
 // number of entries to add to db
 const numOfEntries = 100;
 
 // companies data
-const seedCompany = () => {
+const seedCompanies = () => {
     for (let i = 0; i < numOfEntries; i++) {
         // data categories
         let companyName = faker.company.companyName();
@@ -20,7 +20,7 @@ const seedCompany = () => {
         // insert entries into database
         insertCompanies(companyName, companySuffix, suffix, catchPhrase, companyImage, companyAddressStreet, companyAddressState)
             .then(res => {
-                console.log('company data added to database');
+                console.log('Company data added to database');
             })
             .catch(err => {
                 console.log(`Error adding company data to database: ${err}`);
@@ -28,14 +28,37 @@ const seedCompany = () => {
     }
 };
 
+const seedProducts = () => {
+    for (let i = 0; i < numOfEntries; i++) {
+        // data categories
+        let productName = faker.commerce.productName();
+        let department = faker.commerce.department();
+        let productImage = faker.image.technics();
+        let color = faker.commerce.color();
+        let productMaterial = faker.commerce.productMaterial();
+        let productAdj = faker.commerce.productAdjective();
+        let price = Number(faker.commerce.price());
+
+        console.log(productMaterial, productAdj, department)
+
+        insertProducts(productName, department, productImage, color, productMaterial, productAdj, price)
+            .then(res => {
+                console.log('Product added to database');
+            })
+            .catch(err => {
+                console.log(`Error adding product data to database: ${err}`);
+            });
+    }
+};
+
 // truncate tables before seeding
-truncateTables('api_limiter.companies')
+truncateTables('api_limiter.companies', 'api_limiter.products')
     .then(() => {
-
         // ==== run seed functions ====
-
         //company data
-        seedCompany();
+        seedCompanies();
+        //product data
+        seedProducts();
     })
     .catch(err => {
         console.log(`Error seeding database: ${err}`);
