913438e8616e22a234999d18a3ea679c15c317ff
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-07 09:18:51 -0600

    added select by params func

--- server/models/companies.js
+++ server/models/companies.js
@@ -1,10 +1,29 @@
 const { dbConnection } = require('../../database/config.js');
 
+// select all, no params
 module.exports.selectAllCompanies = () => {
     return new Promise((resolve, reject) => {
         let selectAllQuery = 'SELECT * FROM api_limiter.companies';
     
         dbConnection.query(selectAllQuery, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+};
+
+// select data in column by params
+module.exports.selectCompaniesByParams = (colName, ...params) => {
+    return new Promise((resolve, reject) => {
+        let query = {
+            text: ``,
+            values: []
+        };
+
+        dbConnection(query, (err, res) => {
             if (err) {
                 reject(err);
             } else {
