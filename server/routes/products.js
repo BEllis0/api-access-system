98b9350176cf1eb580e2e924f7a456305224f043
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-07 09:19:15 -0600

    added all route connections

---
+++ server/routes/products.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const productControllers = require('../controllers/products.js');
+
+// all products
+router.get('/', productControllers.products.all);
+
+// by product name
+router.get('/:name', productControllers.products.searchBy.name);
+
+// by price range
+router.get('/price', productControllers.products.searchBy.priceRange);
+
+// by color
+router.get('/:color', productControllers.products.searchBy.color);
