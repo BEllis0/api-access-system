89f1ab1928abf654c3ff03760ed71f62515c725e
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-19 15:29:15 -0600

    separated user sql table information:

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,20 +1,5 @@
 CREATE SCHEMA api_limiter;
 
--- users
-
-CREATE TABLE api_limiter.users (
-    id SERIAL PRIMARY KEY,
-    date_added TIMESTAMP,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
-    username VARCHAR(200) NOT NULL UNIQUE,
-    email_address VARCHAR(200) NOT NULL,
-    membership_tier VARCHAR(50) default 'free',
-    api_key VARCHAR(200) UNIQUE,
-    total_calls INT,
-    alotted_calls INT
-);
-
 -- table for each API call
 
 CREATE TABLE api_limiter.api_calls (
---
+++ sql/users.sql
@@ -0,0 +1,14 @@
+-- users
+
+CREATE TABLE api_limiter.users (
+    id SERIAL PRIMARY KEY,
+    date_added TIMESTAMP,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    username VARCHAR(200) NOT NULL UNIQUE,
+    email_address VARCHAR(200) NOT NULL,
+    membership_tier VARCHAR(50) default 'free',
+    api_key VARCHAR(200) UNIQUE,
+    total_calls INT,
+    alotted_calls INT
+);
