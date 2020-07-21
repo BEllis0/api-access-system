f28f69cbae5ad1dd9bb4df9668209b791d0c42cf
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-20 19:54:02 -0600

    added password into insert statement

--- server/models/users.js
+++ server/models/users.js
@@ -4,7 +4,7 @@ const { dbConnection } = require('../../database/config.js');
 
 
 
-module.exports.addUser = (firstName, lastName, username, email, membership, apiKey) => {
+module.exports.addUser = (firstName, lastName, username, password, email, membership = 'free', apiKey) => {
     return new Promise((resolve, reject) => {
         let addUserQuery = {
             text: `INSERT INTO api_limiter.users(
@@ -12,14 +12,16 @@ module.exports.addUser = (firstName, lastName, username, email, membership, apiK
                 last_name,
                 email_address,
                 username,
+                password,
                 membership_tier,
                 api_key
-            ) VALUES ($1, $2, $3, $4, $5, $6)`,
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
             values: [
                 `${firstName}`,
                 `${lastName}`,
                 `${email}`,
                 `${username}`,
+                `${password}`,
                 `${membership}`,
                 `${apiKey}`
             ]
