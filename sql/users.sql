e307e70feebd335f04214abde71632b5dc3f1da9
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-20 19:54:35 -0600

    added password field

--- sql/users.sql
+++ sql/users.sql
@@ -6,6 +6,7 @@ CREATE TABLE api_limiter.users (
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     username VARCHAR(200) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
     email_address VARCHAR(200) NOT NULL,
     membership_tier VARCHAR(50) default 'free',
     api_key VARCHAR(200) UNIQUE,
