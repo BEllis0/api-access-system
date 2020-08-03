73e757c30e3d9c28e569fcf1c257cf7b05ada235
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-02 23:02:22 -0600

    fixed bug in query

--- api_key_validation.js
+++ api_key_validation.js
@@ -4,14 +4,12 @@ module.exports.apiKeyValidation = (apiKey) => {
     return new Promise((resolve, reject) => {
 
         let query = `SELECT * FROM api_limiter.api_keys WHERE api_key = '${apiKey}';`;
-        console.log('QUERYYYY', query);
-        dbConnection(query, (err, res) => {
+        
+        dbConnection.query(query, (err, res) => {
             if (err) {
-                console.log('ERRROROROROROR')
                 reject(err);
             } else {
-                console.log('WORKEDDDDDDD')
-                resolve(res);
+                resolve(res.rows[0]);
             }
         });
     });
