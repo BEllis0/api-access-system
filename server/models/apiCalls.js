a396f648a466a006e62f828813adf7755de564cb
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-08 00:10:35 -0600

    models to get and post api calls

---
+++ server/models/apiCalls.js
@@ -0,0 +1,39 @@
+const { dbConnection } = require('../../database/config.js');
+
+// get all api calls from user
+module.exports.getApiCalls = userID => {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM api_limiter.api_calls WHERE user_requested = ${userID}`;
+
+        dbConnection.query(query, (err, res) => { 
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    });
+};
+
+// post api call
+module.exports.postApiCalls = (userID, endpoint) => {
+    return new Promise((resolve, reject) => {
+        const query = `INSERT INTO api_limiter.api_calls(
+            _endpoint,
+            success,
+            user_requested
+        ), VALUES(
+            '${endpoint}',
+            true,
+            ${userID}
+        )`;
+
+        dbConnection.query(query, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+};
