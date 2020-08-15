12865e841eeda2abd02b613dca8f7f6b7af1ddac
Brandon Ellis <brandonellis.email@gmail.com>
2020-08-14 19:50:48 -0600

    implemented free tier api-call-by-minute function:

--- account_tracker.js
+++ account_tracker.js
@@ -1,4 +1,4 @@
-const { getApiCallsByDate, postApiCalls } = require('./server/models/apiCalls.js');
+const { getApiCallsByDate, getApiCallsByMinute, postApiCalls } = require('./server/models/apiCalls.js');
 const { dateConverter } = require('./dateConverter.js');
 
 // ==========================================
@@ -13,50 +13,62 @@ module.exports.accountTracker = (userID, membershipTier, endpoint) => {
 
         // set the number of calls available for free and premium accounts per day
         let freeLimit = 5;
-        let premiumLimit = 50;
+        let premiumLimit = 1000;
         
         // hold the number of api calls from user per day
         let numOfCalls;
 
-        // GET all API calls from user
-        await getApiCallsByDate(userID)
-            .then(response => {
-                // handle first API call from user; none in database
-                if (response.rowCount === 0) {
-                    numOfCalls = 1;
-                    return;
-                } else {
+        // handle free account limits
+        if (membershipTier === 'free') {
+            // free accounts are limited by minute
+            await getApiCallsByMinute(userID)
+                .then(response => {
+                    // set number of call reference
                     numOfCalls = response.rowCount;
+                    if (numOfCalls === 0) {
+                        // if no calls yet
+                        numOfCalls = 1;
+                        return;
 
-                    // handle free account tier
-                    if (membershipTier === 'free') {
-                        // if more than free allotted calls today
-                        if (numOfCalls > freeLimit) {
-                            console.log('API call limit hit');
-                            // resolve to false to hit limit error
-                            resolve({ success: false });
-                        } else {
-                            resolve({ success: true });
-                        }
+                    } else if (numOfCalls > freeLimit) {
+                        console.log('API call limit hit');
+                        // resolve to false to hit limit error
+                        resolve({ success: false });
                     }
-            
-                    // handle premium account tier
-                    if (membershipTier === 'premium') {
-                        // if more than premium allotted calls today
-                        if (numOfCalls > premiumLimit) {
-                            console.log('API call limit hit');
-                            // resolve to false to hit limit error
-                            resolve({ success: false });
-                        } else {
-                            resolve({ success: true });
-                        }
+                })
+                .catch(err => {
+                    console.log('Error getting API calls for user', err);
+                    reject({ success: false });
+                });
+        }
+
+        // handle premium account limits
+        if (membershipTier === 'premium') {
+            // premium account is limited by day
+            await getApiCallsByDate(userID)
+                .then(response => {
+                    // set number of call reference
+                    numOfCalls = response.rowCount;
+                    // if no calls yet
+                    if (response.rowCount === 0) {
+                        numOfCalls = 1;
+                        return;
+
+                    } else if (numOfCalls > premiumLimit) {
+                        console.log('API call limit hit');
+                        // resolve to false to hit limit error
+                        resolve({ success: false });
                     }
-                }
-            })
-            .catch(err => {
-                console.log('Error getting API calls for user', err);
-                reject({ success: false });
-            });
+                })
+                .catch(err => {
+                    console.log('Error getting API calls for user', err);
+                    reject({ success: false });
+                });
+        }
+
+        // =============================
+        // IF ALL ABOVE CHECKS ARE PASSED
+        // =============================
 
         // POST API call
         await postApiCalls(userID, endpoint)
